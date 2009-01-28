50ba5b05da659e6470e6f02e0fa002082e843edb
jpsaman <jpsaman@3589dd83-fcde-432d-8375-59f0e9bff529>
2009-01-28 13:32:43 +0000

    Fix library naming. libdvbcsa was intented by the authors instead of libcsa.

--- trunk/test/testbitslice.c
+++ trunk/test/testbitslice.c
@@ -1,19 +1,19 @@
 /*
 
-    This file is part of libcsa.
+    This file is part of libdvbcsa.
 
-    libcsa is free software; you can redistribute it and/or modify it
+    libdvbcsa is free software; you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
-    libcsa is distributed in the hope that it will be useful, but
+    libdvbcsa is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with libcsa; if not, write to the Free Software Foundation,
+    along with libdvbcsa; if not, write to the Free Software Foundation,
     Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
