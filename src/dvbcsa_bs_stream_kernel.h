815405b5b7bf61275bc4192d5f04e9a7f44d0c1c
glenvt18 <glenvt18@gmail.com>
2015-05-05 13:37:43 +0300

    stream cipher: refactoring
    1. Move stream cipher kernel code into a separate file included twice.
    Don't duplicate code.
    2. Store stream cipher registers in a structure passed as an argument.
    3. Use virtual shift registers for A and B, avoid copying on each round.

---
+++ src/dvbcsa_bs_stream_kernel.h
@@ -0,0 +1,23 @@
+#ifndef DVBCSA_BS_STREAM_KERNEL_H_
+#define DVBCSA_BS_STREAM_KERNEL_H_
+
+#include "dvbcsa_bs.h"
+
+struct dvbcsa_bs_stream_regs_s {
+  dvbcsa_bs_word_t A[32 + 10][4];
+  dvbcsa_bs_word_t B[32 + 10][4];
+  dvbcsa_bs_word_t X[4];
+  dvbcsa_bs_word_t Y[4];
+  dvbcsa_bs_word_t Z[4];
+  dvbcsa_bs_word_t D[4];
+  dvbcsa_bs_word_t E[4];
+  dvbcsa_bs_word_t F[4];
+  dvbcsa_bs_word_t sb[64];
+  dvbcsa_bs_word_t cb[64];
+  dvbcsa_bs_word_t p;
+  dvbcsa_bs_word_t q;
+  dvbcsa_bs_word_t r;
+};
+
+#endif
+
